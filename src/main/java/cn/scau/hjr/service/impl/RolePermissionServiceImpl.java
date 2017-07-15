ec82cb3121c879525e696e8e9a141de7e5f088f9
天梯 <1807768141@qq.com>
2017-07-15 10:21:31 +0800

    权限分配做好

--- src/main/java/cn/scau/hjr/service/impl/RolePermissionServiceImpl.java
+++ src/main/java/cn/scau/hjr/service/impl/RolePermissionServiceImpl.java
@@ -3,9 +3,11 @@ package cn.scau.hjr.service.impl;
 import cn.scau.hjr.dao.RolePermissionMapper;
 import cn.scau.hjr.model.RolePermission;
 import cn.scau.hjr.service.RolePermissionService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2017/7/14 0014.
@@ -34,5 +36,27 @@ public class RolePermissionServiceImpl implements RolePermissionService {
     public RolePermission selectByRoleIdAndPermission(RolePermission rolePermission) {
         RolePermission temp=rolePermissionMapper.selectByRoleIdAndPermission(rolePermission);
         return temp;
+    }
+
+    @Override
+    public RolePermission selectByPrimaryKey(Integer id) {
+
+        RolePermission rolePermission=null;
+        rolePermission=rolePermissionMapper.selectByPrimaryKey(id);
+        return rolePermission;
+
+    }
+
+    @Override
+    public ArrayList<RolePermission> selectByRoleId(Integer id){
+        ArrayList<RolePermission> rolePermissions=null;
+        rolePermissions=rolePermissionMapper.selectByRoleId(id);
+        return rolePermissions;
+    }
+
+    @Override
+    public int insert(RolePermission record) {
+        rolePermissionMapper.insert(record);
+        return 0;
     }
 }
