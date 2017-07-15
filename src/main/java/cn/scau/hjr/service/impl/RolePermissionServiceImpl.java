a36695d5d217d5c2c9be1970a18de7050301f4f7
天梯 <1807768141@qq.com>
2017-07-15 09:28:37 +0800

    角色管理界面做好

--- src/main/java/cn/scau/hjr/service/impl/RolePermissionServiceImpl.java
+++ src/main/java/cn/scau/hjr/service/impl/RolePermissionServiceImpl.java
@@ -3,12 +3,14 @@ package cn.scau.hjr.service.impl;
 import cn.scau.hjr.dao.RolePermissionMapper;
 import cn.scau.hjr.model.RolePermission;
 import cn.scau.hjr.service.RolePermissionService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 /**
  * Created by Administrator on 2017/7/14 0014.
  */
+@Service
 public class RolePermissionServiceImpl implements RolePermissionService {
     @Resource
     private RolePermissionMapper rolePermissionMapper;
